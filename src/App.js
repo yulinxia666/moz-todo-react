ce0c95dc0b90fbbba5b2c88f83b4ed7357fa432c
ylx <1029476752@qq.com>
2025-02-06 19:10:29 +0800

    lesson10

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
+
+export default function App(props) {
+    const [tasks, setTasks] = useState(props.tasks);
+
+    function addTask(name) {
+        const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+        setTasks([...tasks, newTask]);
+      }
+      
+    const taskList = tasks.map((task) => (
+        <Todo 
+        id={task.id} 
+        name={task.name} 
+        completed={task.completed} 
+        key={task.id}/>
+      ));
+      
+    return (
+        <div className="todoapp stack-large">
+            <h1>TodoMatic</h1>
+            <Form onSubmit={addTask} />
+
+            <div className="filters btn-group stack-exception">
+            import React from "react";
+            <FilterButton FilterName="All"/>
+            <FilterButton FilterName="Active"/>
+            <FilterButton FilterName="Completed"/>
+
+            </div>
+            <h2 id="list-heading">3 tasks remaining</h2>
+            <ul
+                role="list"
+                className="todo-list stack-large stack-exception"
+                aria-labelledby="list-heading">
+                {taskList}
+
+            </ul>
+        </div>
+    );
+}
